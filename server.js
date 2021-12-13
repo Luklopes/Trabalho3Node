bb02518154f6a0b9f045773e50e38d271859fcfc
Luklopes <lucasbllopes@hotmail.com>
2021-12-13 09:56:18 -0300

    finalizado

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 var app = express()
+const bodyParser = require('body-parser')
 
-const lista_produtos = { 
+var lista_produtos = { 
     produtos: [ 
         { id: 1, descricao: "Arroz parboilizado 5Kg", valor: 25.00, marca: "Tio João"  }, 
         { id: 2, descricao: "Maionese 250gr", valor: 7.20, marca: "Helmans"  }, 
@@ -11,7 +12,9 @@ const lista_produtos = {
     ] 
 } 
 
-app.use (express.urlencoded({extended:true}))
+// app.use(express.urlencoded({extended:true}))
+//app.use(express.urlencoded({extended:true}))
+app.use(express.json());
 
 app.use(function(req,res,next){
     console.log(JSON.stringify(req.params))
@@ -27,16 +30,53 @@ app.get('/produtos',function(req,res){
 })
 
 app.get('/produtos/:id',function(req,res){
-    res.json({"id":req.params.id, "descricao":req.params.descricao})
+    console.log("entrou")
+    const {id} = req.params;
+    console.log(id)
+    let idDoida = JSON.stringify(id)
+    console.log(idDoida)
+    console.log(1)
+    let doida2 = Number(req.params)
+    console.log(doida2)
+    let foundUser = lista_produtos.produtos.find((item) => item.id == req.params.id)
+    console.log(foundUser)
+    res.send(foundUser)
 })
+
 app.post('/produtos',function(req,res){
-    res.json({})
+    const descricao = req.body;
+    console.log(req.params);
+    lista_produtos.produtos.push(descricao)
+    res.json(lista_produtos)
+    //res.json({ message: 'Produto Criado com Sucesso!' ,lista_produtos});
 })
+
 app.put('/produtos/:id',function(req,res){
-    res.json({"id":req.params.id, "descricao":req.params.descricao})
+    const {id} = req.params;
+    
+    let foundUser = lista_produtos.produtos.find((item) => item.id == req.params.id)
+    let descricao = req.body.descricao;
+    let valor = req.body.valor;
+    let marca = req.body.marca;
+    console.log(descricao)
+    foundUser.descricao = descricao
+    foundUser.valor = valor
+    foundUser.marca = marca
+
+    res.json({ message: 'Produto Editado com Sucesso!' ,lista_produtos});
+    
 })
 app.delete('/produtos/:id',function(req,res){
-    res.json({"id":req.params.id, "descricao":req.params.descricao})
+    const {id} = req.params;
+    console.log(id)
+    
+    let index = lista_produtos.produtos.findIndex((item) => item.id == req.params.id);
+   
+    lista_produtos.produtos.splice(index, 1);
+  
+    res.json({ message: 'Produto Excluido com Sucesso!' ,lista_produtos});
+
+    //res.json({"id":req.params.id, "descricao":req.body.descricao})
 })
 
 let porta =3000
